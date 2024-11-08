93953cb744aadcc071095649759e16560a7b7342
unknown <narongkorn.mon@dome.tu.ac.th>
2024-11-08 19:19:21 +0700

    use mysql2/promise&delete Sequelize

--- routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const requestController = require('../controllers/requestController');
-
-// Route for creating a new request
-router.post('/requests', requestController.createRequest);
-
-module.exports = router;
